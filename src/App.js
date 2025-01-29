4e2425a8bdd6ac3660fbd09260427270b62b3114
ezelllow <EZELLLOWQW29.23@ichat.sp.edu.sg>
2025-01-30 00:26:09 +0800

    Added the homePage page with its own navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar'; // Default Navbar
+import HomePageNavbar from './components/homeNavbar'; // New Navbar for Homepage
 import './App.css';
 import Home from './components/pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Use Routes instead of Switch
-import Services from './components/pages/Services';
+import HomePage from './components/pages/homePage';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar/>
-        <Routes>
-          {/* Use element prop with JSX */}
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/sign-up" element={<SignUp />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <ConditionalNavbar /> {/* This will display the correct navbar */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/homePage" element={<HomePage />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
+}
+
+// Function to determine which Navbar to show
+function ConditionalNavbar() {
+  const location = useLocation(); // Get current route
+
+  if (location.pathname === '/homePage') {
+    return <HomePageNavbar />; // Show a different navbar for homePage
+  }
+
+  return <Navbar />; // Show the default navbar for other pages
 }
 
 export default App;
