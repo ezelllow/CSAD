c3ed8da9bf363ec59ce2726672f1e5d478507b9e
ezelllow <EZELLLOWQW29.23@ichat.sp.edu.sg>
2025-01-30 22:32:00 +0800

    map view only

--- src/components/pages/map.js
+++ src/components/pages/map.js
@@ -1,35 +1,21 @@
-import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const mapContainerStyle = {
-  width: '100%',
-  height: '600px',
-};
-
-const center = {
-  lat: 1.3521, // Default center (Singapore)
-  lng: 103.8198,
-};
+const containerStyle = { width: '100%', height: '90vh' };
+const center = { lat: 1.3521, lng: 103.8198 };
 
 const locations = [
-  { id: 1, name: "Location 1", lat: 1.290270, lng: 103.851959, link: "/location1" },
-  { id: 2, name: "Location 2", lat: 1.340270, lng: 103.731959, link: "/location2" },
+  { id: 1, name: "Bukit Batok Fridge", lat: 1.3496, lng: 103.7499, path: "/bukitbatok" },
+  { id: 2, name: "Tampines Fridge", lat: 1.3541, lng: 103.9457, path: "/tampines" },
 ];
 
-const MapPage = () => {
+export default function MapPage() {
   return (
-    <LoadScript googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY">
-      <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={12}>
+    <LoadScript googleMapsApiKey="AIzaSyDygiyK5pjtseTqWT2EotjWfiD28SBV-ZU">
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
         {locations.map((loc) => (
-          <Marker
-            key={loc.id}
-            position={{ lat: loc.lat, lng: loc.lng }}
-            onClick={() => window.location.href = loc.link}
-          />
+          <Marker key={loc.id} position={{ lat: loc.lat, lng: loc.lng }} onClick={() => window.location.href = loc.path} />
         ))}
       </GoogleMap>
     </LoadScript>
   );
-};
-
-export default MapPage;
+}
